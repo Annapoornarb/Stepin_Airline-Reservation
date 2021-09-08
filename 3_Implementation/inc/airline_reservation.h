2badc3c9e6a076f1bf2f613a7d38cfd75527cf88
Annapoornarb <44547681+Annapoornarb@users.noreply.github.com>
2021-09-08 16:49:42 +0530

    Add files via upload

---
+++ 3_Implementation/inc/airline_reservation.h
@@ -0,0 +1,43 @@
+/**
+ * @file airline_booking.h
+ * @author Annapurna Bisanalli
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef AIR_H
+#define AIR_H
+//#define size 5
+#include "math.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+
+typedef struct NODE
+{
+	int reg_no;
+	int passport_no;
+	int age;
+	char name[20];
+	char destination[20];
+	struct NODE *next;
+} node;
+
+node* deq();
+void create();
+int reserve(node*);
+int cancel(int);
+void enq(node*);
+void display();
+
+
+node *start;
+node *front;
+node *rear;
+int count=0;
+int num=0;
+
+#endif 
